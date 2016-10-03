d957f9a114d03108266a8f61defe8e2f343fa8f8
Joao Pedro <j.p.a.caixeta@gmail.com>
2016-10-03 09:45:20 -0300

    Colocando pecas no tabuleiro

---
+++ projeto/xadrez/Rei.cs
@@ -0,0 +1,13 @@
+﻿using tabuleiro;
+
+namespace projetoxadrez {
+    class Rei : Peca{
+
+        public Rei(Tabuleiro tab, Cor cor) : base (tab, cor) {
+        }
+
+        public override string ToString() {
+            return "R";
+        }
+    }
+}
---
+++ projeto/xadrez/Torre.cs
@@ -0,0 +1,13 @@
+﻿using tabuleiro;
+
+namespace projetoxadrez {
+    class Torre : Peca {
+
+        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor) {
+        }
+
+        public override string ToString(){
+            return "T";
+        }
+    }
+}
